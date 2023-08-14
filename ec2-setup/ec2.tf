74c1f1ddf1213d0887a9abc64920f40ba75ac0a5
lukehegarty <ldhegarty@gmail.com>
2023-08-14 22:58:50 +0100

    added ec2 setup details

--- ec2-setup/ec2.tf
+++ ec2-setup/ec2.tf
@@ -2,9 +2,6 @@
 # Create Private Key  #
 #######################
 
-## TODO Assign instance to default VPC and possibly create security group?
-## Move git folder up a level and manage all from parent folder.
-
 # downside to this approach is that the private key is stored in state, not the end of the world if state 
 # is encrypted at rest inside of s3 bucket. Creates a .pem which is macOs compatible
 
@@ -26,6 +23,30 @@ resource "aws_key_pair" "A4L_key_pair" {
   }
 }
 
+#########################
+# Create Security Group #
+#########################
+
+resource "aws_security_group" "aws_sg" {
+  name = "security group from terraform"
+
+  ingress {
+    description = "SSH from the internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 ###################
 # Create Instance #
 ###################
@@ -37,14 +58,14 @@ module "ec2_instance" {
 
   ami = data.aws_ami.amazon_linux.id
 
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.A4L_key_pair.key_name
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.A4L_key_pair.key_name
+  monitoring                  = true
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.aws_sg.id]
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = "management"
   }
 }
